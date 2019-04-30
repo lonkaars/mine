53f5e0ae7a05c963e249bcba17a75ac1b5862de4
Loekaars <l.leblansch@gmail.com>
2019-04-30 19:04:25 +0200

    Added comments and removed 1 line function
    - Added comments
    - Removed function that was only called once and contained 1 line of code

--- pain.py
+++ pain.py
@@ -3,38 +3,28 @@ from threading import Timer
 import time
 import re
 import pyaudio
-import wave
 from playsound import playsound
-threshold = 4 # number of ctrl presses per second to activate
-
-
-
-def playMine():
-    playsound('mine.wav')
+threshold = 4 # Number of ctrl presses per second to activate
 
 capture = []
 
 def parseSpam():
-    str = ''.join(capture)
+    str = ''.join(capture) # Make a string out of the array
+    regex = re.compile(r'(True)+|(False)+') # Make the variable 'regex' a regex
+    strrearr = regex.findall(str) # Match the string against the regex and return all matches in an array
+    count = len(strrearr) / 2 # Divide by 2 because the array also contains key up events
+    if(count >= threshold): # Check if key was pressed more than the threshold
+        playsound('mine.wav') # Play sound
 
-    regex = re.compile(r'(True)+|(False)+')
-    strrearr = regex.findall(str)
-    count = len(strrearr) / 2
-    if(count >= threshold):
-        playMine()
-
-while True:
+while True: # Main loop
     if(len(capture) == 0 and keyboard.is_pressed('ctrl')):
-        capture.append("True")
-
-    if(len(capture) >= 1 and len(capture) << 20):
+        capture.append("True") # First capture
+    elif(len(capture) >= 1 and len(capture) << 20): # This pushes the key state
         if keyboard.is_pressed('ctrl'):
             capture.append("True")
         else:
             capture.append("False")
-
-    if(len(capture) == 20):
-        parseSpam()
-        capture = []
-
-    time.sleep(0.05)
+    elif(len(capture) == 20):
+        parseSpam() # Check if key was pressed more than the threshold
+        capture = [] # Reset the capture array
+    time.sleep(0.05) # Delay between captures
